17202055e20b50b3e526a6c3da29301358ec34bb
whonore <wolfhonore@gmail.com>
2020-08-03 20:00:50 -0400

    Fix misplaced comma

--- ino/readdata/readdata.ino
+++ ino/readdata/readdata.ino
@@ -29,11 +29,11 @@ struct encoder_t {
 }
 
 struct encoder_t encoders[] = {
-    ENCODER(38, 39, 40)
+    ENCODER(38, 39, 40),
     ENCODER(34, 35, 36),
     ENCODER(30, 31, 32),
     ENCODER(26, 27, 28),
-    ENCODER(22, 23, 24),
+    ENCODER(22, 23, 24)
 };
 #define NENCODERS (sizeof(encoders) / sizeof(encoders[0]))
 
