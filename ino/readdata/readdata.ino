57ca3c26f0dd0ae253a125f84f502340d125d022
whonore <wolfhonore@gmail.com>
2022-06-12 13:22:38 -0400

    Use Adafruit_Seesaw for encoders

--- ino/readdata/readdata.ino
+++ ino/readdata/readdata.ino
@@ -6,78 +6,73 @@
  * Read data from sensors and send to SuperCollider over serial.
  */
 
+#include <Adafruit_seesaw.h>
+
 // Number of bytes needed to fit nbits bits
 #define NBYTES(nbits) ((nbits) / 8) + ((nbits) % 8 != 0)
 
-const uint32_t DELAY_MS = 1; // msec to wait between sending data
-const uint32_t TIMEOUT_MS = 1000; // maximum time (msec) to send no data
+const uint32_t DELAY_MS = 10; // msec to wait between sending data
+const uint32_t TIMEOUT_MS = 500; // maximum time (msec) to send no data
 const uint32_t TIMEOUT_LOOPS = TIMEOUT_MS / DELAY_MS;
 const byte SYNC = 255; // Special value to mark end of send
-
-enum Dir {
-    NONE,
-    LEFT,
-    RIGHT,
-};
 
 struct button_t {
     const byte pin;
     byte last;
 };
-#define BUTTON(p) { .pin = p, .last = LOW }
+#define BUTTON(p) { .pin = p, .last = HIGH }
 
 struct encoder_t {
-    const byte A;
-    const byte B;
-    struct button_t button;
-    byte Alast;
+    Adafruit_seesaw encoder;
+    byte last;
 };
-#define ENCODER(a, b, but) { \
-    .A = (a), .B = (b), .button = BUTTON(but), \
-    .Alast = LOW, \
-}
+#define ENCODER(enc) { .encoder = enc, .last = HIGH }
 
+const byte SS_SWITCH = 24;
+const byte SEESAW_BASE_ADDR = 0x36;
 static struct encoder_t encoders[] = {
-    ENCODER(38, 39, 40),
-    ENCODER(34, 35, 36),
-    ENCODER(30, 31, 32),
-    ENCODER(26, 27, 28),
-    ENCODER(22, 23, 24)
+    ENCODER(Adafruit_seesaw()),
+    ENCODER(Adafruit_seesaw()),
+    ENCODER(Adafruit_seesaw()),
+    ENCODER(Adafruit_seesaw()),
+    ENCODER(Adafruit_seesaw()),
 };
 #define NENCODERS (sizeof(encoders) / sizeof(encoders[0]))
 
 static struct button_t buttons[] = {
-    BUTTON(37),  // Idx1
-    BUTTON(33),  // Idx2
-    BUTTON(29),  // Idx3
-    BUTTON(25),  // Idx4
-    BUTTON(42),  // Left
-    BUTTON(43),  // Right
-    BUTTON(41)   // Stomp
+    BUTTON(8),   // Idx1
+    BUTTON(9),   // Idx2
+    BUTTON(10),  // Idx3
+    BUTTON(11),  // Idx4
+    BUTTON(6),   // Left
+    BUTTON(7),   // Right
+    BUTTON(12)   // Stomp
 };
 #define NBUTTONS (sizeof(buttons) / sizeof(buttons[0]))
 
-//                     Sync       Encoder Offs    Encoder Btns Btns
-const byte PKT_BYTES = 1 + NBYTES(2 * NENCODERS + NENCODERS +  NBUTTONS);
+//                     Sync       Encoder Offs        Encoder Btns Btns
+const byte PKT_BYTES = 1 + NBYTES(8 * NENCODERS + NENCODERS +  NBUTTONS);
 
 static uint32_t timer = 0;
 
 void setup() {
     for (byte i = 0; i < NENCODERS; i++) {
-        pinMode(encoders[i].A, INPUT);
-        pinMode(encoders[i].B, INPUT);
-        pinMode(encoders[i].button.pin, INPUT);
+        // Reverse encoder order to go left-right
+        encoders[i].encoder.begin(SEESAW_BASE_ADDR + (NENCODERS - i - 1));
+        encoders[i].encoder.pinMode(SS_SWITCH, INPUT_PULLUP);
+        encoders[i].encoder.setGPIOInterrupts(((uint32_t) 1) << SS_SWITCH, true);
+        encoders[i].encoder.enableEncoderInterrupt();
     }
     for (byte i = 0; i < NBUTTONS; i++) {
-        pinMode(buttons[i].pin, INPUT);
+        pinMode(buttons[i].pin, INPUT_PULLUP);
     }
 
-    Serial.begin(19200);
+    Serial.begin(115200);
     while (!Serial) {}
 }
 
 void loop() {
-    enum Dir offset[NENCODERS];
+    int8_t offset[NENCODERS];
     bool pressed[NENCODERS + NBUTTONS];
     byte pkt[PKT_BYTES];
 
@@ -85,8 +80,8 @@ void loop() {
 
     // Read encoder and button states
     for (byte i = 0; i < NENCODERS; i++) {
-        offset[i] = readEncoder(&encoders[i]);
-        pressed[i] = isPressed(&encoders[i].button);
+        offset[i] = readEncoderOff(&encoders[i]);
+        pressed[i] = isEncoderPressed(&encoders[i]);
     }
     for (byte i = 0; i < NBUTTONS; i++) {
         pressed[NENCODERS + i] = wasPressed(&buttons[i]);
@@ -95,25 +90,9 @@ void loop() {
     // Build a packet
     memset(pkt, 0, PKT_BYTES);
     byte bit_off = 0;
-    // Two bits per encoder (00 = no change, 10 = left, 01 = right)
-    for (byte i = 0; i < NENCODERS; i++, bit_off += 2) {
-        byte idx = bit_off / 8;
-        byte bit_idx = bit_off % 8;
-        switch (offset[i]) {
-        case NONE:
-            bitWrite(pkt[idx], bit_idx, 0);
-            bitWrite(pkt[idx], bit_idx + 1, 0);
-            break;
-        case LEFT:
-            bitWrite(pkt[idx], bit_idx, 0);
-            bitWrite(pkt[idx], bit_idx + 1, 1);
-            break;
-        case RIGHT:
-            bitWrite(pkt[idx], bit_idx, 1);
-            bitWrite(pkt[idx], bit_idx + 1, 0);
-            break;
-        }
-    }
+    // One byte per encoder (signed 8-bit offset)
+    memcpy(pkt, offset, NENCODERS);
+    bit_off += 8 * NENCODERS;
     // One bit per button (0 = unpressed, 1 = pressed)
     for (byte i = 0; i < NENCODERS + NBUTTONS; i++, bit_off += 1) {
         byte idx = bit_off / 8;
@@ -135,27 +114,31 @@ void loop() {
         Serial.write(SYNC);
     }
 
+    yield();
     delay(DELAY_MS);
 }
 
-static enum Dir readEncoder(struct encoder_t *encoder) {
-    enum Dir off = NONE;
-    byte Acur = digitalRead(encoder->A);
-    if ((encoder->Alast == HIGH) && (Acur == LOW)) {
-        off = (digitalRead(encoder->B) == HIGH ? RIGHT : LEFT);
-    }
-    encoder->Alast = Acur;
-    return off;
+static uint8_t readEncoderOff(struct encoder_t *encoder) {
+    int32_t off = -encoder->encoder.getEncoderDelta();
+    // Shift negatives by 1 to avoid clash with -1 and SYNC
+    off = (off < 0) ? off - 1 : off;
+    return (uint8_t) constrain(off, INT8_MIN, INT8_MAX);
+}
+
+static byte readEncoderButton(struct encoder_t *encoder) {
+    byte st = encoder->encoder.digitalRead(SS_SWITCH);
+    encoder->last = st;
+    return st;
+}
+
+static bool isEncoderPressed(struct encoder_t *encoder) {
+    return readEncoderButton(encoder) == LOW;
 }
 
 static byte readButton(struct button_t *button) {
     byte st = digitalRead(button->pin);
     button->last = st;
     return st;
-}
-
-static bool isPressed(struct button_t *button) {
-    return readButton(button) == LOW;
 }
 
 static bool wasPressed(struct button_t *button) {
